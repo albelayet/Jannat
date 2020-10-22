d34fda3bbd8c0b80d6531aa165b005fde5f30dc8
albelayet <72737448+albelayet@users.noreply.github.com>
2020-10-22 11:25:14 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,9 @@
+$( document ).ready( function() {
+    var typed = new Typed(".type", {
+        strings: ["JANNAT", "A DESIGNER", " A DEVELOPER"],
+        typeSpeed: 200,
+        backSpeed: 300,
+        loop: true,
+      });
+    });
+
